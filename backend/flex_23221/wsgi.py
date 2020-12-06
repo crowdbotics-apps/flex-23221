c2ad49f389a7f829b6c3e999a1960dfb83d0ef8e
Crowdbotics <team@crowdbotics.com>
2020-12-06 14:24:38 +0000

    Update models

--- backend/flex_23221/wsgi.py
+++ backend/flex_23221/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flex_23221.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "flex_23221.settings")
 
 application = get_wsgi_application()
